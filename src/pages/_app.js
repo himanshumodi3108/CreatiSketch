8343e520e49f45cc4ce1258ff49ddb2436c1e1e1
Himanshu Kumar Modi <himanshukumar.modi2021@vitstudent.ac.in>
2025-11-20 01:34:30 +0530

    Add vercel web analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,14 @@ import { Provider } from 'react-redux'
 import { store } from '@/store'
 import ErrorBoundary from '@/components/ErrorBoundary'
 import '@/styles/globals.css'
+import { Analytics } from '@vercel/analytics/next';
 
 export default function App({ Component, pageProps }) {
   return (
     <ErrorBoundary>
       <Provider store={store}>
         <Component {...pageProps} />
+        <Analytics />
       </Provider>
     </ErrorBoundary>
   )
